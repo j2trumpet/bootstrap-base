a35e1ddc45b0ff8300e35c0ae7ecdbeaa0a77e23
John Plastow <j2trumpet@gmail.com>
2019-03-18 15:21:01 -0700

    Upgrading to 4.3.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require("gulp-notify");
 
-gulp.task('scss', function() {
-    return gulp.src('assets/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(''));
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(notify('Styles Processed.'))
+    .pipe(gulp.dest('./'))
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('assets/scss/**/*.scss',['scss']);
+    gulp.watch('./assets/scss/**/*.scss',['sass']);
 });
